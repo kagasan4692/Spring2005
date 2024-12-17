d49dc7be1bc40b4f427d6b213bef07d42d101024
kagasan <kagasan@yandex.ru>
2024-12-17 15:43:35 +0300

    add class csvFileHandler

--- Sinergia-projects/modul_03_project/src/point01/CsvFileHandler.java
+++ Sinergia-projects/modul_03_project/src/point01/CsvFileHandler.java
@@ -26,34 +26,6 @@ public class CsvFileHandler extends FileHandler {
         return content.toString();
     }
 
-    public String[][] readCsv() throws IOException {
-        String[][] records = new String[100][10]; // Ограничение на 100 строк и 10 колонок
-        int rowIndex = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] splitLine = line.split(",");
-                records[rowIndex] = new String[splitLine.length];
-                for (int i = 0; i < splitLine.length; i++) {
-                    records[rowIndex][i] = splitLine[i];
-                }
-                rowIndex++;
-            }
-        }
-        return records;
-    }
-
-    public void writeCsv(String[][] data) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            for (int i = 0; i < data.length; i++) {
-                if (data[i] != null) {
-                    writer.write(String.join(",", data[i]));
-                    writer.newLine();
-                }
-            }
-        }
-    }
-
     @Override
     public String writeSumByMont(String[][][] data) {
         StringBuilder result = new StringBuilder();
@@ -81,4 +53,121 @@ public class CsvFileHandler extends FileHandler {
         strNumber = strNumber.replace(',', '.');
         return Double.parseDouble(strNumber);
     }
+
+    public void processTransactionFile(EnterTheData enteredData) throws IOException {
+        TransactionsDataCsvArray dataByMonth = new TransactionsDataCsvArray();
+        MonthlyTransactionArrays monthlyTransaction = new MonthlyTransactionArrays();
+
+//        writeNewData(enteredData);
+
+        String[] dataRead = createDataArray(this.read());
+        for (int i = 0; i < dataRead.length; i++) {
+            System.out.println(dataRead[i]);
+        }
+
+        String[][] allTransaction = dataByMonth.getTransactionsArray(dataRead);
+
+
+        String[][][] transactionByMonth = monthlyTransaction.splitByMonthAndYear(allTransaction);
+
+        String sumByMonth = this.writeSumByMont(transactionByMonth);
+
+        this.writeDataToFile(sumByMonth);
+
+        System.out.println();
+        System.out.println("Новые данные добавлены в файле: " + this.filePath);
+    }
+
+    public static String[] createDataArray(String input) {
+        String[] substrings = new String[countClosingBraces(input)];
+
+        int startIndex = 0;
+        int j = 0;
+        for (int i = 0; i < input.length(); i++) {
+
+            if (input.charAt(i) == '\n') {
+                if (startIndex != 0) {
+                    substrings[j] = input.substring(startIndex, i);
+                    j++;
+                }
+                startIndex = i + 1;
+            }
+        }
+
+        return substrings;
+    }
+
+    public static int countClosingBraces(String input) {
+        int count = 0;
+        int startIndex = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '\n') {
+                if (startIndex != 0) {
+                    count++;
+                }
+                startIndex = i + 1;
+            }
+        }
+
+        return count;
+    }
+
+    private void writeNewData(EnterTheData enteredData) {
+        String[] dataTransaction = enteredData.enteredData();
+
+        String input = dataTransaction[0] + "," + dataTransaction[1] + "," + dataTransaction[2];
+
+
+        String inputLine = input.trim();
+
+        this.writeDataToFile(inputLine);
+    }
+
+    public void writeDataToFile(String data) {
+        try {
+            String header = "Текущая дата и время" + "," + "Описание сделки" + "," + "Сумма сделки" + "\n";
+
+            if (this.read().equals("")) {
+                this.write(header + data);
+            } else {
+                this.write(this.read() + data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class TransactionsDataCsvArray {
+    private static String[][] getDataArray(String[] data) {
+        String[][] result = new String[data.length][3];
+        for (int i = 0; i < data.length; i++) {
+            String[] transaction = new String[3];
+
+            int startIndex = 0;
+            int endIndex = data[i].indexOf(',', startIndex);
+            System.out.println(data[i].substring(startIndex, endIndex - 9).trim());
+            transaction[1] = data[i].substring(startIndex, endIndex - 9);
+
+            startIndex = endIndex + 1;
+            endIndex = data[i].indexOf(',', startIndex);
+            System.out.println(data[i].substring(startIndex, endIndex).trim());
+            transaction[0] = data[i].substring(startIndex, endIndex).trim();
+
+            startIndex = endIndex + 1;
+            endIndex = data[i].length();
+
+            System.out.println(data[i].substring(startIndex, endIndex).trim());
+            transaction[2] = data[i].substring(startIndex, endIndex).trim();
+
+            result[i] = transaction;
+        }
+
+        return result;
+    }
+
+    public static String[][] getTransactionsArray(String[] data) {
+        return getDataArray(data);
+    }
+
 }
--- Sinergia-projects/modul_03_project/src/point01/TxtFileHandler.java
+++ Sinergia-projects/modul_03_project/src/point01/TxtFileHandler.java
@@ -64,7 +64,7 @@ public class TxtFileHandler extends FileHandler {
     }
 
     public void processTransactionFile(EnterTheData enteredData) throws IOException {
-        TransactionsDataArray dataByMonth = new TransactionsDataArray();
+        TransactionsDataTxtArray dataByMonth = new TransactionsDataTxtArray();
         MonthlyTransactionArrays monthlyTransaction = new MonthlyTransactionArrays();
 
         writeNewData(enteredData);
@@ -141,6 +141,49 @@ public class TxtFileHandler extends FileHandler {
         }
 
         return count;
+    }
+
+}
+
+class TransactionsDataTxtArray {
+    private static String[][] getDataArray(String[] data) {
+        String[][] result = new String[data.length][3];
+        for (int i = 0; i < data.length; i++) {
+            String[] transaction = new String[3];
+            transaction[0] = getDescription(data[i]);
+            transaction[1] = getDate(data[i]);
+            transaction[2] = getSum(data[i]);
+
+            result[i] = transaction;
+        }
+
+        return result;
+    }
+
+    private static String getDescription(String inputValue) {
+        int startIndex = inputValue.indexOf("Описание сделки") + 17;
+        int endIndex = inputValue.indexOf(';', startIndex);
+
+        return inputValue.substring(startIndex, endIndex).trim();
+    }
+
+
+    private static String getDate(String inputValue) {
+        int startIndex = inputValue.indexOf("Текущая дата и время") + 21;
+        int endIndex = inputValue.indexOf(';', startIndex) - 9;
+
+        return inputValue.substring(startIndex, endIndex).trim();
+    }
+
+    private static String getSum(String inputValue) {
+        int startIndex = inputValue.indexOf("Сумма сделки") + 14;
+        int endIndex = inputValue.indexOf(';', startIndex);
+
+        return inputValue.substring(startIndex, endIndex).trim();
+    }
+
+    public static String[][] getTransactionsArray(String[] data) {
+        return getDataArray(data);
     }
 
 }

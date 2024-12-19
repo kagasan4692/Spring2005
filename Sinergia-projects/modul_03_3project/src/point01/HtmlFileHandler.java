d2048796c8140fdf452131e30a0ea2927414f17b
kagasan <kagasan@yandex.ru>
2024-12-20 00:16:12 +0300

    Add functional for HTML improve...

--- Sinergia-projects/modul_03_3project/src/point01/HtmlFileHandler.java
+++ Sinergia-projects/modul_03_3project/src/point01/HtmlFileHandler.java
@@ -36,8 +36,8 @@ public class HtmlFileHandler extends FileHandler {
     @Override
     public String writeSumByMont(String[][][] data) {
         StringBuilder result = new StringBuilder();
-//        result.append("<html>\n<head>\n<title>Transaction Summary</title>\n</head>\n<body>\n");
-        result.append("<h3>Итоги по месяцам</h3>\n<table border='1'>\n<tr><th>Месяц</th><th>Год</th><th>Сумма</th></tr>\n");
+        result.append("<h3>Итоги по месяцам</h3>\n<table border='1'>\n" +
+                "<tr><th>Месяц</th><th>Год</th><th>Сумма</th></tr>\n");
 
         for (int i = 0; i < data.length; i++) {
             if (data[i] != null) {
@@ -53,7 +53,7 @@ public class HtmlFileHandler extends FileHandler {
                 }
                 result.append("<tr><td>").append(monthName).append("</td><td>")
                         .append(year).append("</td><td>")
-                        .append(String.format("%.2f", sum)).append("</td></tr>\n");
+                        .append(String.format("%.2f", sum).replace(",", ".")).append("</td></tr>\n");
             }
         }
 
@@ -71,27 +71,37 @@ public class HtmlFileHandler extends FileHandler {
         MonthlyTransactionArrays monthlyTransaction = new MonthlyTransactionArrays();
 
         String header = "<html lang=\"ru\">\n<head>\n<meta charset=\"utf-8\">\n<title>Transactions</title>\n" +
-                "</head>\n<body>\n<h3>Список транзакций</h3>\n<table border='1'>\n" +
+                "</head>\n<body>\n<h3>Список транзакций</h3>\n<table border='1' class=\"general-table\">\n" +
                 "<tr><th>Дата</th><th>Описание</th><th>Сумма</th></tr>\n";
 
         String closeTable = "</table>\n";
 
         String footer = "</body>\n</html>";
 
-        String enteredRow = writeNewData(enteredData);
-        write(header + enteredRow + closeTable);
-
+        if (this.read().equals("")) {
+            String enteredRow = writeNewData(enteredData);
+            write(header + enteredRow + closeTable);
+        } else {
+            String oldRows = getRows(this.read());
+            String enteredRow = writeNewData(enteredData);
+            write(header + oldRows + "\n" + enteredRow + closeTable);
+        }
         String[] dataRead = createDataArray(this.read());
-
         String[][] allTransactions = dataByMonth.getTransactionsArray(dataRead);
         String[][][] transactionsByMonth = monthlyTransaction.splitByMonthAndYear(allTransactions);
-
         String sumByMonth = writeSumByMont(transactionsByMonth);
-        System.out.println(sumByMonth);
-
         writeToFile(sumByMonth + footer);
+        System.out.println("Данные обновлены в файле: " + filePath);
+    }
 
-//        System.out.println("Данные обновлены в файле: " + filePath);
+    public static String getRows(String string) {
+        int startIndex = string.indexOf("general-table");
+        int endIndex = string.indexOf("</table>", startIndex);
+
+        String quote = string.substring(startIndex, endIndex).trim();
+        String[] quoteTable = quote.split("</th></tr>");
+
+        return quoteTable[1].trim();
     }
 
     public static String[] createDataArray(String input) {
@@ -136,8 +146,9 @@ public class HtmlFileHandler extends FileHandler {
         return newRow;
     }
 
-    public void writeToFile(String data) throws IOException {
+    public void writeToFile(String data){
         try {
+
             this.write(this.read() + data);
         } catch (IOException e) {
             e.printStackTrace();
@@ -150,9 +161,6 @@ class TransactionsDataHtmlArray {
     private static String[][] getDataArray(String[] data) {
 
         String[][] result = new String[counterLines(data)][3];
-        for (int i = 0; i < data.length; i++) {
-            System.out.println(data[i]);
-        }
         int j = 0;
         for (int i = 1; i < data.length; i++) {
             if (data[i].contains("<td>")) {

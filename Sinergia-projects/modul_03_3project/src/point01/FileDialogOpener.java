bff90f5690754ba61898416ce9593e29d1ea6ec9
kagasan <kagasan@yandex.ru>
2024-12-24 11:54:13 +0300

    Add functional for CSV improve...-6

--- Sinergia-projects/modul_03_3project/src/point01/FileDialogOpener.java
+++ Sinergia-projects/modul_03_3project/src/point01/FileDialogOpener.java
@@ -6,18 +6,13 @@ import java.io.File;
 public class FileDialogOpener {
 
     public File openFileDialog() {
-        // Создаем диалоговое окно выбора файла
         JFileChooser fileChooser = new JFileChooser();
-
-        // Устанавливаем заголовок окна
         fileChooser.setDialogTitle("Выберите файл для работы");
 
-        // Открываем диалоговое окно
         int result = fileChooser.showOpenDialog(null);
 
-        // Обрабатываем выбор пользователя
         if (result == JFileChooser.APPROVE_OPTION) {
-            // Получаем выбранный файл
+
             File selectedFile = fileChooser.getSelectedFile();
             System.out.println("Выбран файл: " + selectedFile.getAbsolutePath());
             return selectedFile;

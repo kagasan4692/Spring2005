9dac14fe497c44b401ba66c6b30b5d8ed51b2df0
kagasan <kagasan@yandex.ru>
2025-01-11 18:56:19 +0300

    task 43 exercise 01

--- Sinergia-projects/task43/src/Exercise02.java
+++ Sinergia-projects/task43/src/Exercise02.java
@@ -1,10 +1,10 @@
 public class Exercise02 implements NewsFetcher {
     public static void main(String[] args) {
         Exercise02 app = new Exercise02();
-        String keyword = "science";
+        String category = "science";
 
-        System.out.println("Запрос новостей для категории: " + keyword);
-        String response = app.fetchNews(keyword);
+        System.out.println("Запрос новостей для категории: " + category);
+        String response = app.fetchNews(category);
 
         System.out.println("\nРезультат запроса (JSON):");
         System.out.println(response);
--- Sinergia-projects/task43/src/Exercise03.java
+++ Sinergia-projects/task43/src/Exercise03.java
@@ -1,12 +1,14 @@
 public class Exercise03 implements NewsFetcher {
+//  Поскольку указанный в задании API в данный момент возвращает ответ, что сервис временно не доступен,
+//  я использовал другой новостной API.
     public static void main(String[] args) {
         Exercise03 app = new Exercise03();
 
         String[] categories = {"science", "business", "sports", "world", "technology", "automobile"};
 
-        for (String category : categories) {
-            System.out.println("Запрос новостей для категории: " + category);
-            String response = app.fetchNews(category);
+        for (int i = 0; i < categories.length; i++) {
+            System.out.println("Запрос новостей для категории: " + categories[i]);
+            String response = app.fetchNews(categories[i]);
 
             System.out.println("\nРезультат запроса (JSON):");
             System.out.println(response);

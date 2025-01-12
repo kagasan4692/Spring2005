4a2ce88ff02ba44f210433d432c18813485e7039
kagasan <kagasan@yandex.ru>
2025-01-12 17:02:09 +0300

    Task42

---
+++ Sinergia-projects/task44/src/SongLyrics.java
@@ -0,0 +1,11 @@
+public abstract class SongLyrics {
+
+    public abstract String getLyrics(String songTitle);
+
+    public String formatLyrics(String lyrics) {
+        if (lyrics == null || lyrics.isEmpty()) {
+            return "Lyrics not found.";
+        }
+        return "Song Lyrics:\n" + lyrics;
+    }
+}
